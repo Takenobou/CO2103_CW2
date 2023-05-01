1cc4ea369bfd4e20ee19e71846595e90b3b3599c
A A Martin <alexmartin153@live.co.uk>
2023-05-01 20:49:16 +0100

    SessionRestController.java

--- part1/src/main/java/edu/leicester/co2103/controller/SessionRestController.java
+++ part1/src/main/java/edu/leicester/co2103/controller/SessionRestController.java
@@ -1,5 +1,62 @@
 package edu.leicester.co2103.controller;
 
+import edu.leicester.co2103.domain.Session;
+import edu.leicester.co2103.repo.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/sessions")
 public class SessionRestController {
 
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Session>> listSessions() {
+        List<Session> sessions = (List<Session>) sessionRepository.findAll();
+        return new ResponseEntity<>(sessions, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Session> createSession(@RequestBody Session session) {
+        Session createdSession = sessionRepository.save(session);
+        return new ResponseEntity<>(createdSession, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Session> getSession(@PathVariable Long id) {
+        Optional<Session> session = sessionRepository.findById(id);
+        if (session.isPresent()) {
+            return new ResponseEntity<>(session.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Session> updateSession(@PathVariable Long id, @RequestBody Session sessionUpdates) {
+        Optional<Session> session = sessionRepository.findById(id);
+        if (session.isPresent()) {
+            Session updatedSession = session.get();
+            updatedSession.setWeekNumber(sessionUpdates.getWeekNumber());
+            updatedSession.setType(sessionUpdates.getType());
+            updatedSession.setContent(sessionUpdates.getContent());
+            sessionRepository.save(updatedSession);
+            return new ResponseEntity<>(updatedSession, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSession(@PathVariable Long id) {
+        sessionRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
