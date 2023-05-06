6358e321a5bc4a1d5251f93d54cc1a8b0328239b
A A Martin <alexmartin153@live.co.uk>
2023-05-06 20:27:23 +0100

    HTTP status and error repsonse for SessionRestController.java

--- part1/src/main/java/edu/leicester/co2103/controller/SessionRestController.java
+++ part1/src/main/java/edu/leicester/co2103/controller/SessionRestController.java
@@ -3,11 +3,13 @@ package edu.leicester.co2103.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import edu.leicester.co2103.ErrorInfo;
 import edu.leicester.co2103.domain.Session;
 import edu.leicester.co2103.repo.ConvenorRepository;
 import edu.leicester.co2103.repo.ModuleRepository;
 import edu.leicester.co2103.repo.SessionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,24 +29,36 @@ public class SessionRestController {
     }
 
     @GetMapping
-    public List<Session> getAllSessions(@RequestParam(value = "convenor", required = false) Long id, @RequestParam(value = "module", required = false) String code) {
+    public ResponseEntity<?> getAllSessions(@RequestParam(value = "convenor", required = false) Long id, @RequestParam(value = "module", required = false) String code) {
         List<Session> sessions = (List<Session>) sessionRepository.findAll();
 
         if (id != null) {
-            sessions = sessions.stream()
-                    .filter(session -> convenorRepository.findById(id).get().getModules().stream()
-                            .anyMatch(module1 -> module1.getSessions().contains(session)))
-                    .collect(Collectors.toList());
+            if (convenorRepository.findById(id).isPresent()) {
+                sessions = sessions.stream()
+                        .filter(session -> convenorRepository.findById(id).get().getModules().stream()
+                                .anyMatch(module1 -> module1.getSessions().contains(session)))
+                        .collect(Collectors.toList());
+            } else {
+                return new ResponseEntity<>(new ErrorInfo("Convenor with id " + id + " not found"), HttpStatus.NOT_FOUND);
+            }
         }
 
         if (code != null) {
-            sessions = sessions.stream()
-                    .filter(session -> moduleRepository.findById(code).get().getSessions().contains(session))
-                    .collect(Collectors.toList());
+            if (moduleRepository.findById(code).isPresent()) {
+                sessions = sessions.stream()
+                        .filter(session -> moduleRepository.findById(code).get().getSessions().contains(session))
+                        .collect(Collectors.toList());
+            } else {
+                return new ResponseEntity<>(new ErrorInfo("Module with code " + code + " not found"), HttpStatus.NOT_FOUND);
+            }
         }
 
-        return sessions;
+        if (sessions.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(sessions, HttpStatus.OK);
     }
+
 
     @DeleteMapping
     public ResponseEntity<?> deleteSession() {
