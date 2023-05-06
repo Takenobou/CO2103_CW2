487957c8f3cd6dc4634a3e208af3ce03b4da0994
A A Martin <alexmartin153@live.co.uk>
2023-05-06 19:49:24 +0100

    - start of endpoint 19-20

--- part1/src/main/java/edu/leicester/co2103/controller/SessionRestController.java
+++ part1/src/main/java/edu/leicester/co2103/controller/SessionRestController.java
@@ -1,39 +1,49 @@
 package edu.leicester.co2103.controller;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
-import edu.leicester.co2103.ErrorInfo;
-import edu.leicester.co2103.domain.Convenor;
-import edu.leicester.co2103.domain.Module;
+import edu.leicester.co2103.domain.Session;
 import edu.leicester.co2103.repo.ConvenorRepository;
 import edu.leicester.co2103.repo.ModuleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import edu.leicester.co2103.domain.Session;
 import edu.leicester.co2103.repo.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("sessions")
 public class SessionRestController {
 
     private final SessionRepository sessionRepository;
+    private final ModuleRepository moduleRepository;
+    private final ConvenorRepository convenorRepository;
 
     @Autowired
     public SessionRestController(SessionRepository sessionRepository, ModuleRepository moduleRepository, ConvenorRepository convenorRepository) {
         this.sessionRepository = sessionRepository;
+        this.moduleRepository = moduleRepository;
+        this.convenorRepository = convenorRepository;
     }
 
     @GetMapping
-    public List<Session> getAllSessions() {
-        return (List<Session>) sessionRepository.findAll();
+    public List<Session> getAllSessions(@RequestParam(required = false) Long convenor, @RequestParam(required = false) String module) {
+        List<Session> sessions = (List<Session>) sessionRepository.findAll();
+
+        if (convenor != null) {
+            sessions = sessions.stream()
+                    .filter(session -> convenorRepository.findById(convenor).get().getModules().stream()
+                            .anyMatch(module1 -> module1.getSessions().contains(session)))
+                    .collect(Collectors.toList());
+        }
+
+        if (module != null) {
+            sessions = sessions.stream()
+                    .filter(session -> moduleRepository.findById(module).get().getSessions().contains(session))
+                    .collect(Collectors.toList());
+        }
+
+        return sessions;
     }
 
     @DeleteMapping
